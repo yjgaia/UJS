d9c9f87f54a2db82541bd038e97bff8563f13fe7
Hanul <mr@hanul.co>
2014-09-10 01:06:51 +0900

    add URI_MATCHER

---
+++ SRC/COMMON/UTIL/URI_MATCHER.js
@@ -0,0 +1,94 @@
+/**
+ * URI matcher class
+ */
+global.URI_MATCHER = URI_MATCHER = CLASS({
+
+	init : function(inner, self, format) {
+		'use strict';
+		//REQUIRED: format
+
+		var
+		// Check class
+		Check = CLASS({
+
+			init : function(inner, self, uri) {
+				//REQUIRED: uri
+
+				var
+				// uri parts
+				uriParts = uri.split('/'),
+
+				// is matched
+				isMatched,
+
+				// uri parmas
+				uriParams = {},
+
+				// find.
+				find = function(format) {
+
+					var
+					// format parts
+					formatParts = format.split('/');
+
+					return EACH(uriParts, function(uriPart, i) {
+
+						var
+						// format part
+						formatPart = formatParts[i];
+
+						if (formatPart === '**') {
+							isMatched = true;
+							return false;
+						}
+
+						if (formatPart === undefined) {
+							return false;
+						}
+
+						// find params.
+						if (formatPart.charAt(0) === '{' && formatPart.charAt(formatPart.length - 1) === '}') {
+							uriParams[formatPart.substring(1, formatPart.length - 1)] = uriPart;
+						} else if (formatPart !== '*' && formatPart !== uriPart) {
+							return false;
+						}
+
+						if (i === uriParts.length - 1 && i < formatParts.length - 1 && formatParts[formatParts.length - 1] !== '') {
+							return false;
+						}
+
+					}) === true || isMatched === true;
+				},
+
+				// check is matched.
+				checkIsMatched,
+
+				// get uri params.
+				getURIParams;
+
+				if (CHECK_IS_ARRAY(format) === true) {
+					isMatched = EACH(format, function(format) {
+						return find(format) !== true;
+					}) !== true;
+				} else {
+					isMatched = find(format);
+				}
+
+				self.checkIsMatched = checkIsMatched = function() {
+					return isMatched;
+				};
+
+				self.getURIParams = getURIParams = function() {
+					return uriParams;
+				};
+			}
+		}),
+
+		// check.
+		check;
+
+		self.check = check = function(uri) {
+			return Check(uri);
+		};
+	}
+});
