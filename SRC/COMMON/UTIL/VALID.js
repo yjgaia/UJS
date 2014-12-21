5ca813c09a8d3650f8c0671a151df0e98f9ebbed
Hanul <hanul@hanul.me>
2014-12-21 10:04:14 +0900

    rollback VALID

--- SRC/COMMON/UTIL/VALID.js
+++ SRC/COMMON/UTIL/VALID.js
@@ -604,9 +604,7 @@ global.VALID = VALID = CLASS(function(cls) {
 									}
 								}
 
-								if (notEmpty(value) !== true) {
-									data[attr] = undefined;
-								} else if (typeof value === 'string') {
+								if (notEmpty(value) === true && typeof value === 'string') {
 									if (name === 'integer') {
 										data[attr] = INTEGER(value);
 									} else if (name === 'real') {
