e71e4abcf9b95f723f1387bc29c5eeafdb96ccc1
Hanul <hanul@hanul.me>
2016-05-25 18:47:33 +0900

    no message

--- SRC/NODE/SERVER/SOCKET_SERVER.js
+++ SRC/NODE/SERVER/SOCKET_SERVER.js
@@ -192,7 +192,7 @@ global.SOCKET_SERVER = METHOD({
 				// callback name
 				callbackName;
 				
-				if (conn !== undefined) {
+				if (conn !== undefined && conn.writable === true) {
 					
 					conn.write(STRINGIFY({
 						methodName : params.methodName,
